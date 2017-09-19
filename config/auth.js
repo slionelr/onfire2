e2da859020b749a008d674cac02ecd315e2d89b0
Messi <messi@messi.com>
2017-09-20 01:25:00 +0300

    changed server port to 8080

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID'      : '326622381134694', // your App ID
 		'clientSecret'  : '090677b6d62295e9d977ab11d4410693', // your App Secret
-		'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
+		'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
 	},
 
 	'googleAuth' : {
